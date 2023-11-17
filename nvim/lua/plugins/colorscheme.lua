929e2d3813d32d1185414731b1b91355d9e45c63
rhodee <info@rhodee.us>
2023-11-16 21:01:11 -0600

    remove unused configs (managed by Lazy)

--- nvim/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-  -- Themes
-return {
-    {
-        'EdenEast/nightfox.nvim',
-        opts = {
-          priority = 1000, -- make sure to load this before all the other start plugins
-        }
-    },
-}
