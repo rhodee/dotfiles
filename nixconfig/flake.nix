506f6e28a80d6bca2434f5bf5164a73050620f61
rhodee <info@rhodee.us>
2024-04-05 09:05:51 -0500

    fix: remove incorrect bash integration opt

--- nixconfig/flake.nix
+++ nixconfig/flake.nix
@@ -175,7 +175,6 @@
               # on macOS, you probably don't need this
               bash = {
                 enable = false;
-                enableBashIntegration = true;
                 initExtra = ''
                   # Make Nix and home-manager installed things available in PATH.
                   export PATH=/run/current-system/sw/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
